cb774d1776d23b469a10666ccf37d2532bf9836a
Marcelo <marcelo-h-h@hotmail.com>
2018-10-01 20:37:44 -0300

    First version

--- app/src/main/java/com/example/marcelo/tkl_game_lister_app/MainActivity.java
+++ app/src/main/java/com/example/marcelo/tkl_game_lister_app/MainActivity.java
@@ -2,17 +2,62 @@ package com.example.marcelo.tkl_game_lister_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-
+    private ArrayList<Games> data;
+    private Adapter adapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        recyclerView = (RecyclerView)findViewById(R.id.games_cards_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        loadJson();
+    }
+
+    private void loadJson() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("dl.dropboxusercontent.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        RequestInterface request = retrofit.create(RequestInterface.class);
+        Call<Response> call = request.getJson();
+        call.enqueue(new Callback<Response>() {
+            @Override
+            public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
+                Response jsonResponse = response.body();
+                data = new ArrayList<>(Arrays.asList(jsonResponse.getListaGames()));
+                adapter = new Adapter(data);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<Response> call, Throwable t) {
+                Log.d("Error", t.getMessage());
+            }
+        });
+
     }
 }
